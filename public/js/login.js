d044ed3ff965c3527161894bb091df4a4acc4275
Mukul pathak <mukulpathak4@gmail.com>
2023-10-11 09:38:46 +0530

    Login Frontend

---
+++ public/js/login.js
@@ -0,0 +1,25 @@
+const btn = document.getElementById('submit');
+
+btn.addEventListener('click', loginUser);
+
+async function loginUser(e) {
+    try {
+        e.preventDefault();
+
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        const loginDetails = {
+            email,
+            password
+        };
+
+        const response = await axios.post('http://localhost:3000/login', loginDetails);
+
+        console.log(response);
+        alert(response.data.message);
+
+    } catch (err) {
+        console.log(err);
+    }
+}
