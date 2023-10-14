99c5b2a1930650b089e9d28a9a4c3b5d18136d2c
Mukul pathak <mukulpathak4@gmail.com>
2023-10-14 08:13:41 +0530

    setInterval used for getting msg in Realtime

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,8 +7,8 @@ async function sendMsg() {
         const textMessage = message.value;
         const token = localStorage.getItem('token');
 
-        const response = await axios.post('http://localhost:3000/chats/send', { message: textMessage }, {
-            headers: { "Authorization": token },
+        const response = await axios.post('http://localhost:3000/chats/send', { textMessage }, {
+            headers: { Authorization: token },
         });
 
         // Clear the message input field
@@ -19,26 +19,27 @@ async function sendMsg() {
         console.log(err);
     }
 }
+
 window.addEventListener('DOMContentLoaded', async () => {
     getUserMsgs();
-})
+    setInterval(getUserMsgs, 1000);
+});
 
 async function getUserMsgs() {
     try {
-            // Get messages from the server using a GET request
-            const response = await axios.get(`http://localhost:3000/chats/messages`);
-            let Chats = response.data.textMessages;
-            showUsersChatsOnScreen(Chats);
-        
+        // Get messages from the server using a GET request
+        const response = await axios.get(`http://localhost:3000/chats/messages`);
+        let Chats = response.data.textMessages;
+        showUsersChatsOnScreen(Chats);
     } catch (err) {
         console.log(err);
-        alert(err.response.data.err);
+        alert(err.response.data.error);
     }
-};
+}
 
 function showUsersChatsOnScreen(chats) {
     const token = localStorage.getItem('token');
-    const decodeToken = parseJwt(token);
+    const decodeToken = {}; // Define or get the decoded token
 
     const ul = document.getElementById('main-content');
     ul.style.textAlign = 'center';
@@ -48,19 +49,24 @@ function showUsersChatsOnScreen(chats) {
     const p = document.createElement('p');
     li.append(p);
 
-    if (isValidURL(chats.message)) {
-        p.innerHTML = `${chats.sender} : <img src="${chats.message}" alt="${chats.sender}">`;
-    } else {
-        p.textContent = `${chats.sender} : ${chats.message}`;
-    }
-
     if (chats.userId === decodeToken.userId) {
         if (isValidURL(chats.message)) {
             p.innerHTML = `you : <img src="${chats.message}" alt="${chats.sender}">`;
         } else {
             p.textContent = `you : ${chats.message}`;
         }
+    } else {
+        if (isValidURL(chats.message)) {
+            p.innerHTML = `${chats.sender} : <img src="${chats.message}" alt="${chats.sender}">`;
+        } else {
+            p.textContent = `${chats.sender} : ${chats.message}`;
+        }
     }
 
     ul.append(li);
+}
+
+function isValidURL(str) {
+    const pattern = /^(https?:\/\/)?([\w-]+(\.[\w-]+)+([/?#].*?)?)?$/;
+    return pattern.test(str);
 }
